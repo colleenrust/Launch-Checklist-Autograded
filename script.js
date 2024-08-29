79277afc9854f14c7160f39b92f8317b91e22de3
colleenrust <colleenrust524@gmail.com>
2024-08-29 17:07:26 -0500

    i put all my js in script js for some reason instead of scripthelper. rip. gotta move some stuff around but this is the code before without theform submission working function

--- script.js
+++ script.js
@@ -13,7 +13,43 @@ validateInput(fuelLevel) === "Empty" || validateInput(cargoMass) === "Empty"){
     alert("All fields required!");
     return;
 }
+if(validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || 
+validateInput(fuelLevel) === "Not a Number" || validateInput(cargoMass) === "Not a Number"){
+    alert("Please enter valid information for each field.");
+    return;
 
+}
+
+//update pilot and copilot beotch
+pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+
+let readyForLaunch = true;
+if(Number(fuelLevel) < 10000) {
+    fuelStatus.innerHTML = "Fuel level to low for launch";
+    readyForLaunch = false;
+} else {
+    fuelStatus.innerHTML = "Fuel level high enough for launch";
+
+}
+
+if(Number(cargoMass) > 10000){
+    cargoStatus.innerHTML = "Cargo mass too high for launch";
+    readyForLaunch = false;
+} else {
+    cargoStatus.innerHTML = "Cargo mass low enough for launch";
+
+}
+
+//update launch status now
+if(readyForLaunch){
+    launchStatus.innerHTML = "Shuttle is ready for launch";
+    launchStatus.style.color = "green";
+    faultyItems.style.visibility = "hidden";
+}else {
+    launchStatus.innerHTML = "Shuttle not ready for launch";
+    launchStatus.style.color = "red";
+    faultyItems.style.visibility = "visible";
 }
 
 window.addEventListener("load", function() {
@@ -30,3 +66,4 @@ window.addEventListener("load", function() {
     })
     
  });
+}
